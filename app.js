69596f9d0c60df099dc64b8b3d2656cc34886f55
asagohan2301 <shemomechama@yahoo.co.jp>
2023-09-18 18:39:51 +0900

    ポート番号修正2

--- app.js
+++ app.js
@@ -180,7 +180,7 @@ app.use((err, req, res, next) => {
 });
 
 // 接続 ----------------
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`ポート${port}でリクエスト待ち受け中...`);
 });
